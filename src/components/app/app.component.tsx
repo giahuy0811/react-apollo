1f755aeddd49b021dbf4ab40526edb12b57f84c7
huylg0811 <huylg@vgsholding.com>
2022-06-16 17:28:29 +0700

    init project

---
+++ src/components/app/app.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.component.css'
+import Header from '../header/header.component';
+
+
+const App: React.FC = () => {
+	return <div className='App'>
+      <Header /> 
+  </div>;
+}
+
+export default App;
